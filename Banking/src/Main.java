2ebde282192d3dbba9319b8bb8dbdba604fef8d9
Felly <164373391+H230366Z@users.noreply.github.com>
2024-08-27 22:03:09 +0200

    Add files via upload

--- Banking/src/Main.java
+++ Banking/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Creating an instance of the Bank class
-        Bank myBankAccount = new Bank("VJ", 230357);
+        Bank myBankAccount = new Bank("FM", 230366);
         myBankAccount.display(); // Invoking the display method
     }
 }
